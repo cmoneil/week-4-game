19828a2ccdbeba13195025575aa600b84a153629
Conor O’Neil <conoroneil@stp-er-10-129-185-62.wireless.umn.edu>
2019-01-02 21:07:34 -0600

    Cleaned up code and added comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,122 +1,82 @@
 $(document).ready(function () {
-    // Your code here...
-    var yourNumber = 0;
-    var yourScore = 0;
-    var crystalMin = 1;
-    var crystalMax = 12;
-    var blueCrystal = [];
-    var yellowCrystal = [];
-    var tealCrystal = [];
-    var purpleCrystal = [];
-    var randomNumber = [];
-    var minNumber = 19;
-    var maxNumber = 120;
-    var winCounter = 0;
-    var lossCounter = 0;
-    var crystalValue = 0;
-
     
+    let yourScore = 0;
+    const crystalMin = 1;
+    const crystalMax = 12;
+    let blueCrystal;
+    let yellowCrystal;
+    let tealCrystal;
+    let purpleCrystal;
+    let randomNumber;
+    const minNumber = 19;
+    const maxNumber = 120;
+    let winCounter = 0;
+    let lossCounter = 0;
+    let crystalValue = 0;
+
+    //sets random number to match
+    const randomNumberFromRange = (min, max) =>
+        Math.floor(Math.random() * (max - min + 1) + min);
+    
+    //sets random number for each crystal
+    const randomCrystalNumber = (min, max) =>
+        Math.floor(Math.random() * (max - min + 1) + min);
+    
+    
+    //sets initial state of game
     function startGame() {
         yourScore = 0;
         crystalValue = 0;
         randomNumber = randomNumberFromRange(minNumber, maxNumber);
-        blueCrystal = randomBlueCrystalNumber(crystalMin, crystalMax);
-        yellowCrystal = randomYellowCrystalNumber(crystalMin, crystalMax);
-        tealCrystal = randomTealCrystalNumber(crystalMin, crystalMax);
-        purpleCrystal = randomPurpleCrystalNumber(crystalMin, crystalMax);
-
-        function randomNumberFromRange(min, max) {
-            return Math.floor(Math.random() * (max - min + 1) + min);
-        }
-
-        function randomBlueCrystalNumber(min, max) {
-            return Math.floor(Math.random() * (max - min + 1) + min);
-        }
-        function randomYellowCrystalNumber(min, max) {
-            return Math.floor(Math.random() * (max - min + 1) + min);
-        }
-        function randomTealCrystalNumber(min, max) {
-            return Math.floor(Math.random() * (max - min + 1) + min);
-        }
-        function randomPurpleCrystalNumber(min, max) {
-            return Math.floor(Math.random() * (max - min + 1) + min);
-        }
-
-        
-
-        console.log(randomNumber);
-        console.log(blueCrystal);
-        console.log(yellowCrystal);
-        console.log(tealCrystal);
-        console.log(purpleCrystal);
-
-
+        blueCrystal = randomCrystalNumber(crystalMin, crystalMax);
+        yellowCrystal = randomCrystalNumber(crystalMin, crystalMax);
+        tealCrystal = randomCrystalNumber(crystalMin, crystalMax);
+        purpleCrystal = randomCrystalNumber(crystalMin, crystalMax);
 
         $('#yourNumber').html(randomNumber);
         $('#yourScore').html(yourScore);
         $('#crystalValue').html(crystalValue);
+        
     }
 
-    // $('.crystal').click(function () {
-    //   if (blueCrystal) {
-    // yourScore = yourScore + blueCrystal;
-
-    //}
-    // if (yellowCrystal) {
-    //  yourScore = yourScore + yellowCrystal;
-
-    //}
-
-    //$('.yourScore').html(yourScore);
-    // return;
-    //})
-
+    //assigns functionality to each button
     function assignButtons() {
 
-        //if (yourScore < yourNumber){
         $('.blueCrystal').click(function () {
             yourScore = parseInt(yourScore + blueCrystal);
-            console.log(yourScore);
             $('#yourScore').html(yourScore);
-            //blueCrystal = crystalValue;
-            $('#crystalValue').html(blueCrystal)
-        })
+            $('#crystalValue').html(blueCrystal);
+        });
 
         $('.yellowCrystal').click(function () {
             yourScore = parseInt(yourScore + yellowCrystal);
-            console.log(yourScore);
             $('#yourScore').html(yourScore);
-            $('#crystalValue').html(yellowCrystal)
-        })
+            $('#crystalValue').html(yellowCrystal);
+        });
 
         $('.tealCrystal').click(function () {
             yourScore = parseInt(yourScore + tealCrystal);
-            console.log(yourScore);
             $('#yourScore').html(yourScore);
-            $('#crystalValue').html(tealCrystal)
-
-        })
+            $('#crystalValue').html(tealCrystal);
+        });
 
         $('.purpleCrystal').click(function () {
             yourScore = parseInt(yourScore + purpleCrystal);
-            console.log(yourScore);
             $('#yourScore').html(yourScore);
-            $('#crystalValue').html(purpleCrystal)
-        })
+            $('#crystalValue').html(purpleCrystal);
+        });
         
 
     }
 
-
+    //checks win conditions
     function roundComplete() {
-        console.log('Your score: ' + yourScore + '| Your number: ' + randomNumber);
 
         if( yourScore === randomNumber) {
             winCounter++;
             alert('You win!');
             $('#winCounter').html(winCounter);
             startGame();
-
         }
         else if (yourScore > randomNumber) {
             lossCounter++;
@@ -125,6 +85,7 @@ $(document).ready(function () {
             startGame();
         }
     }
+    
     startGame();
     assignButtons();
     $('.crystal').click(function () {
@@ -132,5 +93,3 @@ $(document).ready(function () {
 
     })
 })
-
-    //$('crystal').click(function () {}
